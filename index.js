34d0c178959ae5437349f6396f64986c42b11ac3
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-08 12:14:21 -0800

    Deleted old code and added new JS code

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const increament = document.getElementById("increament");
-const decreament = document.getElementById("decreament");
-const reset = document.getElementById("reset");
-const myh3 = document.getElementById("myh3");
 
-let count = 0;
+const button1 = document.getElementById("button1");
+const labels1 = document.getElementById("labels1");
+const labels2 = document.getElementById("labels2");
+const labels3 = document.getElementById("labels3");
+const min = 1;
+const max = 6;
+let randomnum1;
+let randomnum2;
+let randomnum3;
 
-increament.onclick = function() {
-    count++;
-    myh3.textContent = count;
-};
+button1.onclick = function() {
+    randomnum1 = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomnum2 = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomnum3 = Math.floor(Math.random() * (max - min + 1)) + min;
+    labels1.textContent=randomnum1;
+    labels2.textContent=randomnum2;
+    labels3.textContent=randomnum3;
 
-decreament.onclick = function() {
-    count--;
-    myh3.textContent = count;
-};
-
-reset.onclick = function() {
-    count = 0;
-    myh3.textContent = count;
 };
