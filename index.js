96335ecd1c6502dd77c4fef488bd340842d932a9
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-12 11:54:41 -0800

    git commit -m "Updated file with new code"

--- index.js
+++ index.js
@@ -1,20 +1,125 @@
+// const mycheckbox=document.getElementById("mycheckbox");
+// const mysubmit=document.getElementById("mysubmit");
+// const myresult=document.getElementById("myresult");
+// const mymastercard=document.getElementById("mymastercard");
+// const myvisa=document.getElementById("myvisa")
+// const mypaypal=document.getElementById("mypaypal");
+// const myresult2=document.getElementById("myresult2");
 
-const button1 = document.getElementById("button1");
-const labels1 = document.getElementById("labels1");
-const labels2 = document.getElementById("labels2");
-const labels3 = document.getElementById("labels3");
-const min = 1;
-const max = 6;
-let randomnum1;
-let randomnum2;
-let randomnum3;
+// mysubmit.onclick = function(){
+//     if(mycheckbox.checked){
+//      myresult.textContent="you are subscribed";
+//     }
+//     else{
+//         myresult.textContent="your are not subscribes";
+//     }
+//     if(myvisa.checked){
+//         myresult2.textContent="you are paying through paypal";
+//     }
+//     else if(mymastercard.checked){
+//        myresult2.textContent="your are paying through mastercard";
+//     }
+//     else if(mypaypal.checked){
+//         myresult2.textContent="you are paypa";
+//     }
+//     else{
+//         myresult.textContent="you are not paying";
+//     }
+// }
+// let age=20;
+// let message=age>18?"you are adult":"not audlt";
+// console.log(message);
+// let time=2
+// let greeting=time>2?"good after noon":"good night";
+// console.log(greeting)
+// let day=1;
+// switch(day){
+//     case 1:
+//       console.log("monday");
+//       break;
+//     case 2:
+//         console.log("tuesday");
+    
+// }
+// let mark=56;
+// let lettergrade;
+// switch(true){
+//     case mark>50:{
+//         lettergrade='A';
+//         break;
+//     }
+   
+//     case  mark<50:{
+//       lettergrade='C';
+//       break;
+//     }
+    
+// }
+// console.log(lettergrade);
+// let username="bro code";
+// let firstname=username.slice(0,username.indexOf("o"));
+// console.log(firstname);
+// let username=window.prompt("enter your name :");
+// let letter=username.indexOf("0");
+// letter=letter.toLowerCase();
 
-button1.onclick = function() {
-    randomnum1 = Math.floor(Math.random() * (max - min + 1)) + min;
-    randomnum2 = Math.floor(Math.random() * (max - min + 1)) + min;
-    randomnum3 = Math.floor(Math.random() * (max - min + 1)) + min;
-    labels1.textContent=randomnum1;
-    labels2.textContent=randomnum2;
-    labels3.textContent=randomnum3;
+// let extaract=username.slice(1);
+// extaract=extaract.toLowerCase();
+// username=letter+extaract;
+// console.log(username);
 
-};
+// let pi=3.14;
+// if(pi==="3.14"){
+//     console.log("this is strictly equlal")
+// }
+// else{
+//     console.log("this is not")
+// }
+// let logedin=false;
+// let username;
+// let password;
+// while(!logedin){
+//     username=window.prompt("enter your name");
+//     password=window.prompt("enter your password")
+   
+//     if(username === "myfirstname" && password === "mypassword"){
+//            logedin=true;
+//            console.log("your are logged in");
+//     }
+//     else{
+//         console.log("your are not logged in");
+//     }
+// }
+
+let min=1;
+let max=100;
+let answer=Math.floor(Math.random()*100);
+
+let attemp=0;
+let guess;
+let running=true;
+
+while(running){
+    let guess = window.prompt("Enter a number between " + min + " and " + max + ":");
+    guess=Number(guess);
+    if(isNaN(guess)){
+        window.alert("not guess type");
+    }
+    else if(guess<min || max<guess){
+        window.alert("this is not in range");
+    }
+    else{
+        attemp++;
+        if(guess<answer){
+            window.alert("to low");
+        }
+        else if(guess>answer){
+            window.alert("to high");
+        }
+        else{
+            window.alert("correct in",{attemp},"tines")
+            running =false;
+        }
+
+    }
+}
